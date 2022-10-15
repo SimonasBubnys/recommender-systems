a71e8de75f2b4bee6c56a1ff7f4d59206c94d7db
IgnacioCadarso <72268339+IgnacioCadarso@users.noreply.github.com>
2022-10-15 18:40:20 +0200

    fix

--- max_marg_relevance.py
+++ max_marg_relevance.py
@@ -1,49 +1,65 @@
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
+import pandas as pd
 
-def recommend(session_list, playlist):
+def recommend(average_session_songs, playlist):
     """
-        Return ranked phrases using MMR. Cosine similarity is used as similarity measure.
-        :param session_list: Songs that the user has listened until now
-        :param playlist: Songs to rank
-        """
-    result = np.zeros(len(session_list))
-    for i in session_list:
-        print(i)
-        result = result + session_list[i]
+            :param average_session_songs: Songs that the user has listened until now
+            :param playlist: Songs to rank
+            """
 
-    return 5
+    result = maximal_marginal_relevance(average_session_songs,playlist)
+    return result
 
-def maximal_marginal_relevance(sentence_vector, phrases, embedding_matrix, lambda_constant=0.5, threshold_terms=10):
+def maximal_marginal_relevance(v1,songs, lambda_constant=0.5, threshold_terms=10):
     """
     Return ranked phrases using MMR. Cosine similarity is used as similarity measure.
-    :param sentence_vector: Query vector
-    :param phrases: list of candidate phrases
-    :param embedding_matrix: matrix having index as phrases and values as vector
+    :param v1: query vector
+    :param songs: matrix having index as songs and values as vector
     :param lambda_constant: 0.5 to balance diversity and accuracy. if lambda_constant is high, then higher accuracy. If lambda_constant is low then high diversity.
     :param threshold_terms: number of terms to include in result set
-    :return: Ranked phrases with score
+    :return: Ranked songs with score
     """
-    # todo: Use cosine similarity matrix for lookup among phrases instead of making call everytime.
     s = []
-    r = sorted(phrases, key=lambda x: x[1], reverse=True)
-    r = [i[0] for i in r]
+    r = list(songs.iloc[:,0])
     while len(r) > 0:
         score = 0
-        phrase_to_add = ''
+        song_to_add = None
         for i in r:
-            first_part = cosine_similarity([sentence_vector], [embedding_matrix.loc[i]])[0][0]
+            row = songs.loc[songs['id'] == i]
+            row = row.to_numpy()
+            first_part = cosine_similarity([v1], [row[0][1:]])
             second_part = 0
             for j in s:
-                cos_sim = cosine_similarity([embedding_matrix.loc[i]], [embedding_matrix.loc[j[0]]])[0][0]
+                row2 = songs.loc[songs['id'] == j[0]]
+                row2 = row2.to_numpy()
+                cos_sim = cosine_similarity([row[0][1:]],[row2[0][1:]])
                 if cos_sim > second_part:
                     second_part = cos_sim
             equation_score = lambda_constant*(first_part)-(1-lambda_constant) * second_part
             if equation_score > score:
                 score = equation_score
-                phrase_to_add = i
-        if phrase_to_add == '':
-            phrase_to_add = i
-        r.remove(phrase_to_add)
-        s.append((phrase_to_add, score))
+                song_to_add = i
+        if song_to_add is None:
+            song_to_add = i
+        r.remove(song_to_add)
+        s.append((song_to_add, score))
     return (s, s[:threshold_terms])[threshold_terms > len(s)]
+
+"""
+EXAMPLE 
+In this implementation it is assumed that the songs will be in a pandas dataframe and the first colummn is the index 
+of the songs, but it can be changed later
+"""
+if __name__ == "__main__":
+    average_session_songs  = np.array([0.63,0.616,1,-8.128,0,0.0309,0.463,0.0408,0.173,0.509,118.65])
+    s1 =  np.array([0,0.23,0.84,1,-8.128,0,0.034,0.99,0.01,0.173,0.509,118.65])
+    s2 = np.array([1,0.23,0.06,1,-8.128,0,0.109,0.99,0.02,0.2,0.7,111.65])
+    s3 = np.array([2,0.25,0.52,1,-8.128,0,0.043,0.45,0.03,0.3,0.9,200.65])
+    s4 = np.array([3,0.89,0.31,1,-8.128,0,0.041,0.22,0.04,0.4,0.1,50.65])
+    playlist = pd.DataFrame([s1,s2,s3,s4])
+    playlist.columns = ['id', '', '', '','','','','','','','','']
+    recommend = recommend(average_session_songs,playlist)
+    print(recommend)
+
+
